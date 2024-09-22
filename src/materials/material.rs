493eca9e61f0e8f641459896edbbc8da1fda854f
Caleb Katzenstein <caleb@calokat.me>
2024-09-21 19:32:19 -0700

    Delete EmptyMaterial

--- src/materials/material.rs
+++ src/materials/material.rs
@@ -17,7 +17,3 @@ pub trait Material: Send + Sync {
     }
     fn scattering_pdf(&self, r_in: &Ray, hit_record: &HitRecord, scattered_ray: &Ray) -> Float {0.0}
 }
-
-pub struct EmptyMaterial {}
-
-impl Material for EmptyMaterial {}
