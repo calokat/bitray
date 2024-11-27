698ad24789a310bf8c604613a520543f539b574b
Caleb Katzenstein <caleb@calokat.me>
2024-11-27 15:24:22 -0500

    More detail, larger image

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ use bitray::hittable::Hittable;
 use bitray::hittable::HittableList;
 use bitray::materials::diffuse_light::DiffuseLightMaterial;
 use bitray::materials::lambert::Lambert;
+use bitray::materials::metal::Metal;
 use bitray::quad::Quad;
+use bitray::sphere::Sphere;
 use bitray::texture::ColorTexture2D;
 use bitray::Vec3;
+
 fn main() {
     let grey_texture = ColorTexture2D {
         color: Color::new(1.0, 1.0, 1.0),
@@ -14,6 +17,10 @@ fn main() {
 
     let green_texture = ColorTexture2D {
         color: Color::new(0.12, 0.45, 0.15),
+    };
+
+    let blue_texture = ColorTexture2D {
+        color: Color::new(0.3, 0.3, 0.8),
     };
 
     let red_texture = ColorTexture2D {
@@ -29,6 +36,7 @@ fn main() {
     let mat_red = Lambert::new(&red_texture);
 
     let mat_lambert = Lambert::new(&grey_texture);
+    let mat_metal = Metal::new(&blue_texture, 0.0);
 
     let mat_light = DiffuseLightMaterial::new(&light_texture);
 
@@ -41,6 +49,8 @@ fn main() {
         let ceiling = Quad::new(Vec3::new(0.0, 555.0, 0.0), Vec3::X * 555.0, Vec3::Z * 555.0, &mat_lambert);
         let light = Quad::new(Vec3::new(213.0,554.0,227.0), Vec3::Z * 105.0, Vec3::X * 130.0, &mat_light);
 
+        let sphere = Sphere::new(Vec3::new(275.0f32, 50.0f32, 275.0f32), 50.0f32, &mat_metal, "Red Sphere".into());
+
         let objects: Vec<&dyn Hittable> = vec![
             &light,
             &floor,
@@ -49,12 +59,13 @@ fn main() {
             &floor,
             &wall_back,
             &ceiling,
+            &sphere,
         ];
         let world: HittableList = HittableList::new(objects);
         // let world = HittableList::new(objects);
         let camera = Camera::new(
-            1.0,
-            600,
+            16.0 / 9.0,
+            1920,
             1500,
             20,
             Vec3::new(278.0, 278.0, -800.0),
